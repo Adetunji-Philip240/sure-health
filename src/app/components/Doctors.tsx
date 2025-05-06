8a6291e01e08c7226ef7671aa5b68d156e3283d3
Adetunji-Philip240 <adewalephilip240@gmail.com>
2025-05-06 01:14:15 +0100

    adjusting the doctor/home page

--- src/app/components/Doctors.tsx
+++ src/app/components/Doctors.tsx
@@ -13,58 +13,60 @@ const Doctors = () => {
         preventive measures, and health awareness.
       </p>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 mt-24">
-        <div className="text-center">
-          <div className="bg-blue-300 p-10 rounded-3xl w-80 block mx-auto mb-4">
-            <Image
-              src="/images/doctor (1).jpg"
-              width={300}
-              height={300}
-              className="block mx-auto"
-              alt="doc"
-            />
-            <p className="text-1xl font-bold">Dr. Robert Philips</p>
-            <small>Neurologist</small>
+      <div className="block w-4/5 mx-auto mt-36">
+        <div className="grid grid-cols-1 md:grid-cols-3 ">
+          <div className="text-center">
+            <div className="bg-blue-300 p-10 rounded-3xl w-80 block mx-auto mb-4">
+              <Image
+                src="/images/doctor (1).jpg"
+                width={300}
+                height={300}
+                className="block mx-auto"
+                alt="doc"
+              />
+              <p className="text-1xl font-bold">Dr. Robert Philips</p>
+              <small>Neurologist</small>
 
-            <button className="block mx-auto mt-3 btn btn-dash">
-              Book Appointment
-            </button>
+              <button className="block mx-auto mt-3 btn btn-dash">
+                Book Appointment
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="text-center">
-          <div className="bg-blue-300 p-10 rounded-3xl w-80 block mx-auto mb-4">
-            <Image
-              src="/images/doctor (2).jpg"
-              width={300}
-              height={300}
-              className="block mx-auto"
-              alt="doc"
-            />
-            <p className="text-1xl font-bold">Dr. Elizabeth Blackwell</p>
-            <small>Cardiologist</small>
+          <div className="text-center">
+            <div className="bg-blue-300 p-10 rounded-3xl w-80 block mx-auto mb-4">
+              <Image
+                src="/images/doctor (2).jpg"
+                width={300}
+                height={300}
+                className="block mx-auto"
+                alt="doc"
+              />
+              <p className="text-1xl font-bold">Dr. Elizabeth Blackwell</p>
+              <small>Cardiologist</small>
 
-            <button className="block mx-auto mt-3 btn btn-dash">
-              Book Appointment
-            </button>
+              <button className="block mx-auto mt-3 btn btn-dash">
+                Book Appointment
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="text-center">
-          <div className="bg-blue-300 p-10 rounded-3xl w-80 block mx-auto mb-4">
-            <Image
-              src="/images/doctor (3).jpg"
-              width={300}
-              height={300}
-              className="block mx-auto"
-              alt="doc"
-            />
-            <p className="text-1xl font-bold">Emily Ben</p>
-            <small>Anesthesiologist</small>
+          <div className="text-center">
+            <div className="bg-blue-300 p-10 rounded-3xl w-80 block mx-auto mb-4">
+              <Image
+                src="/images/doctor (3).jpg"
+                width={300}
+                height={300}
+                className="block mx-auto"
+                alt="doc"
+              />
+              <p className="text-1xl font-bold">Emily Ben</p>
+              <small>Anesthesiologist</small>
 
-            <button className="block mx-auto mt-3 btn btn-dash">
-              Book Appointment
-            </button>
+              <button className="block mx-auto mt-3 btn btn-dash">
+                Book Appointment
+              </button>
+            </div>
           </div>
-        </div>
+        </div>{" "}
       </div>
     </div>
   );
